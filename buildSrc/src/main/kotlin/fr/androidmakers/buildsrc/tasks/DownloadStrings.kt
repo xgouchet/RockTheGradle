843221a1547392978139c5a6115d83e53c6dc4ad
Xavier Gouchet <xavier.gouchet@gmail.com>
2018-04-23 17:52:28 +0200

    Add Input/Output definition on task

--- buildSrc/src/main/kotlin/fr/androidmakers/buildsrc/tasks/DownloadStrings.kt
+++ buildSrc/src/main/kotlin/fr/androidmakers/buildsrc/tasks/DownloadStrings.kt
@@ -5,7 +5,12 @@ import com.natpryce.konfig.getValue
 import com.natpryce.konfig.stringType
 import fr.androidmakers.buildsrc.plugins.RemoteL10nExtension
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFiles
 import org.gradle.api.tasks.TaskAction
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 open class DownloadStrings : DefaultTask() {
@@ -27,7 +32,40 @@ open class DownloadStrings : DefaultTask() {
 
     @TaskAction
     fun doIt() {
-    	val fetcher = RemoteL10nFetcher()
+        val fetcher = RemoteL10nFetcher()
         fetcher.fetchStrings(FuelDownloader(), projectBasePath, configuration)
+    }
+
+    @Input
+    fun getDateInput() : String {
+        val formatter = DateTimeFormatter.ISO_DATE
+        return LocalDateTime.now().format(formatter)
+    }
+
+    @Input
+    fun getHostInput(): String {
+        return configuration.remoteHost
+    }
+
+    @Input
+    fun getExtInputs(): List<String> {
+        val inputs = mutableListOf<String>()
+        configuration.variants?.forEach { v ->
+            v.languages.forEach { l ->
+                inputs.add("${v.name}/$l")
+            }
+        }
+        return inputs
+    }
+
+    @OutputFiles
+    fun getTaskOOutputs(): List<File> {
+        val outputs = mutableListOf<File>()
+        configuration.variants?.forEach { v ->
+            v.languages.forEach { l ->
+                outputs.add(File("$projectBasePath/src/${v.name}/res/values-$l/strings.xml"))
+            }
+        }
+        return outputs
     }
 }
