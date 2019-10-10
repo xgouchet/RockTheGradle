4fe38857bb494836c3427b34e499259124ec7b5e
Xavier Gouchet <xavier.gouchet@gmail.com>
2019-10-10 17:32:17 +0200

    Add Task Inputs and Outputs

--- buildSrc/src/main/kotlin/rocks/mobileera/buildsrc/plugin/GetStringsTask.kt
+++ buildSrc/src/main/kotlin/rocks/mobileera/buildsrc/plugin/GetStringsTask.kt
@@ -1,9 +1,14 @@
 package rocks.mobileera.buildsrc.plugin
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFiles
 import org.gradle.api.tasks.TaskAction
 import rocks.mobileera.buildsrc.utils.Downloader
 import rocks.mobileera.buildsrc.utils.FuelDownloader
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 open class GetStringsTask : DefaultTask() {
@@ -17,6 +22,29 @@ open class GetStringsTask : DefaultTask() {
 
     var root = ""
     var extension = GetStringsExtension()
+
+    @Input
+    fun getLanguagesInputs(): List<String> {
+        return extension.languages.toList()
+    }
+
+    @Input
+    fun getBaseUrlInputs(): String {
+        return extension.baseUrl
+    }
+
+    @OutputFiles
+    fun getTaskOutputs(): List<File> {
+        return extension.languages.map { l ->
+            File("$root/res/values-$l/strings.xml")
+        }
+    }
+
+    @Input
+    fun getDateInput(): String {
+        val formatter = DateTimeFormatter.ISO_DATE
+        return LocalDateTime.now().format(formatter)
+    }
 
 
     @TaskAction fun performTask() {
